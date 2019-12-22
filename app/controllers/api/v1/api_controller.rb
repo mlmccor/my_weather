34a76df34c719dc1238b1461a81249421f684ef4
Matthew McCormack <mmccormack1182@gmail.com>
2019-12-22 16:06:37 -0500

    added Api controller and routes

---
+++ app/controllers/api/v1/api_controller.rb
@@ -0,0 +1,12 @@
+module Api
+  module V1
+    class ApiController < ApplicationController
+
+      def get_weather
+      end
+
+      def get_coordinates
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,8 @@ Rails.application.routes.draw do
   end
 
   root 'welcome#home'
+  get '/api/v1/get_weather', to: 'api/v1/api#get_weather'
+  get '/api/v1/get_coords', to: 'api/v1/api#get_coordinates'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
