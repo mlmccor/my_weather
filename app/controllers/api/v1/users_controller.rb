3569662d7fb6d95e594a6582af29c82c48cac4c7
Matthew McCormack <mmccormack1182@gmail.com>
2020-01-02 10:44:45 -0500

    allowed front-end to communicate with back-end

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,8 @@
 module Api
     module V1
         class UsersController < ApplicationController
-        
+
             def show
-                binding.pry
                 user = User.find(params[:id])
                 render json: user
             end
